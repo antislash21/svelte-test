2d8c74b6e431bc35a6123d95613d3890e3cd99cf
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2020-10-22 11:26:05 +0200

    add settings and dark theme

---
+++ src/routes/settings.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+  export async function preload() {
+    console.log(this);
+    console.log('settings preload');
+  }
+  // utiliser this.fetch() au lieu de fetch() (inconnu si executé côté serveur
+</script>
+
+<script>
+  console.log('settings');
+  let isDarkMode = false;
+  let moninput;
+</script>
+
+<svelte:head>
+  <title>Settings</title>
+  {#if isDarkMode}
+    <!-- load CSS in HEAD -->
+    <link rel="stylesheet" href="dark-theme.css" />
+  {/if}
+</svelte:head>
+<h1>settings</h1>
+<input type="text" bind:this={moninput} />
+<br />
+<button
+  on:click={() => {
+    moninput.focus();
+    isDarkMode = !isDarkMode;
+  }}>dark mode</button>
